21116e890130ac601ad78c809b53c05bbb8971a5
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-06 11:51:43 +0600

    display projects

--- src/components/HomePage/Projects/Projects.tsx
+++ src/components/HomePage/Projects/Projects.tsx
@@ -1,4 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { FaGithub, FaRegEye } from "react-icons/fa";
+
+type TProject = {
+  _id: string;
+  projectName: string;
+  projectImage: string;
+  shortDesc: string;
+  features: {
+    one: string;
+    two: string;
+    three: string;
+  };
+  technology: string;
+  liveLink: string;
+  githubClient: string;
+  githubServer: string;
+};
+
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("projects.json")
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
+  }, []);
+
   return (
     <div className="mx-[5.1%] mt-[80px] md:mt-[120px]">
       <div className="text-center">
@@ -8,6 +38,57 @@ const Projects = () => {
       </div>
 
       {/* show projects */}
+      <div className="grid grid-cols-12 gap-[40px] mt-[40px]">
+        {projects?.slice(0, 3).map((project: TProject) => (
+          <div
+            key={project._id}
+            className="col-span-12 md:col-span-4 custom-shadow p-[12px] rounded-lg space-y-[8px]"
+          >
+            <div className="w-full h-[260px] overflow-hidden">
+              <Image
+                src={project.projectImage}
+                width={500}
+                height={500}
+                alt={project.projectName}
+                className="rounded-t-lg"
+              />
+            </div>
+            <div>
+              <h2 className="text-text text-[18px] md:text-[26px] font-medium">
+                {project.projectName}
+              </h2>
+              <p className="text-text-gray text-[15px]">
+                {project.shortDesc.slice(0, 140)}...
+              </p>
+            </div>
+            <div className="flex justify-between items-center">
+              {/* icons */}
+              <div className="flex gap-[12px]">
+                <a
+                  href={project.liveLink}
+                  target="_blank"
+                  className="text-[20px] hover:text-primary duration-500"
+                >
+                  <FaRegEye />
+                </a>
+
+                <a
+                  href={project.githubClient}
+                  target="_blank"
+                  className="text-[20px] hover:text-primary duration-500"
+                >
+                  <FaGithub />
+                </a>
+              </div>
+
+              {/* details btn */}
+              <button className="custom-shadow font-semibold px-[16px] py-[10px] uppercase text-[14px] rounded-lg cursor-pointer border-b-2 border-primary">
+                Details
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
