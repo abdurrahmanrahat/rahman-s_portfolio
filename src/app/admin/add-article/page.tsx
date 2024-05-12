4f3b0639e606d9f3cdaf8c07bff112879b43901d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 21:06:16 +0600

    reuseable file uploader

--- src/app/admin/add-article/page.tsx
+++ src/app/admin/add-article/page.tsx
@@ -1,5 +1,64 @@
+"use client";
+
+import MYFileUploader from "@/components/Forms/MYFileUploader";
+import MYForm from "@/components/Forms/MYForm";
+import MYInput from "@/components/Forms/MYInput";
+import { FieldValues } from "react-hook-form";
+
 const AddArticle = () => {
-  return <div>AddArticle</div>;
+  const handleAddArticle = (values: FieldValues) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="min-h-screen py-20 w-full max-w-[600px] mx-auto">
+      {/* title */}
+      <div className="text-center">
+        <span className="text-[24px] font-semibold uppercase border-b-2 border-primary">
+          Add Article
+        </span>
+      </div>
+
+      {/* form */}
+      <MYForm
+        onSubmit={handleAddArticle}
+        defaultValues={{
+          image: "",
+          title: "",
+          desc: "",
+        }}
+      >
+        <div className="m-8 space-y-3">
+          <div>
+            <label htmlFor="email" className="block mb-2 text-md">
+              Article Image
+            </label>
+            <MYFileUploader name="image" />
+          </div>
+          <div>
+            <label htmlFor="email" className="block mb-2 text-md">
+              Article Title
+            </label>
+            <MYInput name="value" placeholder="Enter title" />
+          </div>
+          <div>
+            <label htmlFor="email" className="block mb-2 text-md">
+              Article Description
+            </label>
+            <MYInput name="category" placeholder="Enter description" />
+          </div>
+        </div>
+
+        <div className="mx-8">
+          <input
+            className="bg-text w-full rounded-md py-3 text-white cursor-pointer"
+            type="submit"
+            value="Add Article"
+          />
+        </div>
+      </MYForm>
+    </div>
+  );
 };
 
 export default AddArticle;
---
+++ src/components/Forms/MYFileUploader.tsx
@@ -0,0 +1,23 @@
+import { Controller, useFormContext } from "react-hook-form";
+
+const MYFileUploader = ({ name }: { name: string }) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, value, ...field } }) => (
+        <input
+          {...field}
+          type="file"
+          value={value?.fileName}
+          onChange={(e) => onChange((e?.target as HTMLInputElement).files?.[0])}
+          className="w-full px-3 py-3 bg-[#EEEEF2] rounded-md focus:outline-none"
+        />
+      )}
+    />
+  );
+};
+
+export default MYFileUploader;
