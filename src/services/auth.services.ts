8ddc665a2bc000fc592d852e678875f94df426e3
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 19:54:59 +0600

    get and remove user from db

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,6 +1,26 @@
-import { setToLocalStorage } from "@/utils/local-storage";
+import { decodedToken } from "@/utils/decodedToken";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-storage";
 
 // store user info to local storage
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
   return setToLocalStorage("accessToken", accessToken);
+};
+
+// get user info from local storage
+export const getUserInfo = () => {
+  const authToken = getFromLocalStorage("accessToken");
+
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return decodedData;
+  }
+};
+
+// remove user
+export const removeUser = () => {
+  return removeFromLocalStorage("accessToken");
 };
