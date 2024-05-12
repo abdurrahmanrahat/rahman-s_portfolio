4ba07883c9639aa8cbc7f4fa795e59606e919dd2
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 16:58:03 +0600

    auth api

---
+++ src/redux/api/authApi.ts
@@ -0,0 +1,22 @@
+import { baseApi } from "./baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (userInfo) => ({
+        url: "/users/register",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
