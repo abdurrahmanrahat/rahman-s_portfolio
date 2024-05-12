39586bc2f0febbeb491f27882dc5e152a3d0dfaf
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 21:40:20 +0600

    article form

--- src/app/admin/add-article/page.tsx
+++ src/app/admin/add-article/page.tsx
@@ -3,6 +3,7 @@
 import MYFileUploader from "@/components/Forms/MYFileUploader";
 import MYForm from "@/components/Forms/MYForm";
 import MYInput from "@/components/Forms/MYInput";
+import MYTextArea from "@/components/Forms/MYTextArea";
 import { FieldValues } from "react-hook-form";
 
 const image_hoisting_token = process.env.NEXT_PUBLIC_imgBB_token;
@@ -11,8 +12,16 @@ console.log(image_hoisting_token);
 const AddArticle = () => {
   const img_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hoisting_token}`;
 
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("en-GB");
+
   const handleAddArticle = (values: FieldValues) => {
-    console.log(values);
+    const readTimeCount =
+      values.desc.length > 400
+        ? "9 Min Read"
+        : values.desc.length > 300
+        ? "6 Min Read"
+        : "5 Min Read";
 
     const formData = new FormData();
     formData.append("image", values.image);
@@ -23,11 +32,17 @@ const AddArticle = () => {
     })
       .then((res) => res.json())
       .then((imgResponse) => {
-        console.log(imgResponse);
         if (imgResponse.success) {
           const image = imgResponse.data.display_url;
-          console.log(image);
+
           // add article to db
+          const newArticle = {
+            image: image,
+            title: values.title,
+            desc: values.desc,
+            date: formattedDate,
+            readTime: readTimeCount,
+          };
         }
       });
   };
@@ -61,13 +76,13 @@ const AddArticle = () => {
             <label htmlFor="email" className="block mb-2 text-md">
               Article Title
             </label>
-            <MYInput name="value" placeholder="Enter title" />
+            <MYInput name="title" placeholder="Enter title" />
           </div>
           <div>
             <label htmlFor="email" className="block mb-2 text-md">
               Article Description
             </label>
-            <MYInput name="category" placeholder="Enter description" />
+            <MYTextArea name="desc" rows={5} placeholder="Enter description" />
           </div>
         </div>
 
---
+++ src/components/Forms/MYTextArea.tsx
@@ -0,0 +1,28 @@
+import { Controller, useFormContext } from "react-hook-form";
+
+type TInputProps = {
+  name: string;
+  placeholder?: string;
+  rows: number;
+};
+
+const MYTextArea = ({ name, placeholder, rows }: TInputProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <textarea
+          {...field}
+          placeholder={placeholder}
+          rows={rows}
+          className="w-full px-3 py-3 bg-[#EEEEF2] rounded-md focus:outline-none"
+        ></textarea>
+      )}
+    />
+  );
+};
+
+export default MYTextArea;
